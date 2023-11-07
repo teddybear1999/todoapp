0d1ff8c19710f7f247a2875c7534bdb0aaeaa7c6
Artur <artfpl@gmail.com>
2023-11-07 10:28:49 +0100

    Add important logs dependency and refactor bash scripts

--- scripts.sh
+++ scripts.sh
@@ -1,48 +1,34 @@
 ### GET TASK WITH ID ###################################################################################################
-for i in {1..1000}; do curl "http://localhost:8080/tasks/$i"; done
+for i in {1..1000}; do curl "http://localhost:8080/tasks/$i"; sleep 0.5; done
 
 ### GET LIST OF COMPLETED TASKS ########################################################################################
-for i in {1..1000}; do curl -X GET "http://localhost:8080/tasks/completed"; done
+for i in {1..100}; do curl -X GET "http://localhost:8080/tasks/completed"; sleep 5; done
 
-### GET LIST OF UNCOMPLETED TASKS ######################################################################################
-for i in {1..1000}; do curl -X GET "http://localhost:8080/tasks/uncompleted"; done
+### GET LIST OF UNCOMPLETED TASKS ########################################################################################
+for i in {1..100}; do curl -X GET "http://localhost:8080/tasks/uncompleted"; sleep 5; done
 
 ### CREATE TASKS WITH POST METHOD#######################################################################################
-for i in {1..1000}; do
-  # generate random date
+for i in {1..10}; do
   month=$(printf "%02d" $((RANDOM % 12 + 1)))
   day=$(printf "%02d" $((RANDOM % 28 + 1)))
   random_date="2023-${month}-${day}"
 
-  # create json body
   json_body="{\"description\":\"description${i}\",\"dueDate\":\"${random_date}\",\"isCompleted\":false}"
 
-  # sending put request
   curl -s -X POST "http://localhost:8080/tasks/" \
        -H "Content-Type: application/json" \
        -d "$json_body"
+  sleep 10
 done
 
 ### UPDATE STATUS TO TRUE ##############################################################################################
-for i in {1..1000}; do
-  curl -s -X PUT "http://localhost:8080/tasks/${i}/completion?isCompleted=true"
-done
+for i in {1..10}; do curl -s -X PUT "http://localhost:8080/tasks/${i}/completion?isCompleted=true"; sleep 1; done
 
 ### UPDATE STATUS TO FALSE #############################################################################################
-for i in {1..1000}; do
-  curl -s -X PUT "http://localhost:8080/tasks/${i}/completion?isCompleted=false"
-done
+for i in {1..1000}; do curl -s -X PUT "http://localhost:8080/tasks/${i}/completion?isCompleted=false"; sleep 1; done
 
 ### UPDATE TASK DESCRIPTION ############################################################################################
-for i in {1..1000}; do
-  json_body="description${i}"
-
-  curl -s -X PUT "http://localhost:8080/tasks/${i}/description" \
-       -H "Content-Type: application/json" \
-       -d "$json_body"
-done
+for i in {1..1000}; do json_body="description${i}"; curl -s -X PUT "http://localhost:8080/tasks/${i}/description" -H "Content-Type: application/json" -d "$json_body"; sleep 1; done
 
 ### DELETE TASKS #######################################################################################################
-for i in {1..100}; do
-  curl -s -X DELETE "http://localhost:8080/tasks/${i}"
-done
+for i in {1..100}; do curl -s -X DELETE "http://localhost:8080/tasks/${i}"; sleep 1; done
